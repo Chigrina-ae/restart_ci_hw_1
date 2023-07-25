840d1004d82224a6f1e4a4ad19b81e0689cbabe2
Anna <aechigrina@gmail.com>
2023-07-25 16:23:35 +0300

    modified:   HW_4/Program.cs

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -49,36 +49,30 @@ Console.WriteLine(SumDigits(Number));
 // 6, 1, 33 -> [6, 1, 33]
 
 
-/*
-int InPut (string message) // метод ввода информации о массиве (его размер и ввод элементов массива)
-{
-    Console.WriteLine(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+int InputNum (string mess) 
+    {
+        Console.WriteLine(mess);
+        return Convert.ToInt32(Console.ReadLine());
+    }
 
 int[] CreateArray(int size)
-{
-    int[]array = new int [size];
-    for(int i=0; i < size; i++)
-    array[i] = InPut($"Введите число массива под индексом {i}");
-    
-    return array;
-
-}
+    {
+        int[]array = new int [size];
+        for(int i = 0; i < size; i++)
+        array[i] = InPut($"Input the number of the array under the index {i}");
+        return array;
+    }
 
 void ShowArray(int[] array)
-{
-    for(int i=0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 
-}
-
-int length = InPut ("Введите количество элементов в массиве");
-
+int length = InputNum ("Input number of elements in the array");
 int[] myArray  = CreateArray(length);
 ShowArray(myArray);
 
-*/
