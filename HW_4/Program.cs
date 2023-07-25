3761c6851219a584f4cf228dc35a21e38087578a
Anna <aechigrina@gmail.com>
2023-07-25 16:10:42 +0300

    modified:   HW_4/Program.cs

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -4,15 +4,15 @@
 // 2, 4 -> 16
 
  int Exponent(int a, int b)
-{
-    int step = a;
-    for( int i = 1; i <= b-1; i++)
-    step  = step * a;
-    return step;
-}
+    {
+        int step = a;
+        for( int i = 1; i <= b-1; i++)
+        step  = step * a;
+        return step;
+    }
+
 Console.Write("Input a base numer "+" ");
 int Base = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("Input a exponent number "+" ");
 int Exp = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(Exponent(Base, Exp));
