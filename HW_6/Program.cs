7d21007b21318b6a485984f365f9eabcdd17be39
Anna <aechigrina@gmail.com>
2023-07-31 00:41:25 +0300

    modified:   HW_6/Program.cs

--- HW_6/Program.cs
+++ HW_6/Program.cs
@@ -5,7 +5,7 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 4
 
-
+/*
 void Positives() 
     {
         int count = 0;
@@ -21,36 +21,36 @@ void Positives()
     }
 
 Positives();
-
+*/
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-/*
-void Crossing()
-{
-    Console.WriteLine("Input b1:");
-    double b1 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Input k1:");
-    double k1 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Input b2:");
-    double b2 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Input k2:");
-    double k2 = Convert.ToDouble(Console.ReadLine());
 
-    if (k1 == k2)
+void Crossing()
     {
-        if (b1 == b2) Console.WriteLine("there is no spoon, all lines are one");
-        else Console.WriteLine("The lines have nothing in common");
+        Console.WriteLine("Input b1:");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Input k1:");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Input b2:");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Input k2:");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+        if (k1 == k2)
+            {
+                if (b1 == b2) Console.WriteLine("there is no spoon, all lines are one");
+                else Console.WriteLine("The lines have nothing in common");
+            }
+        else
+            {
+                double x = (b2 - b1) / (k1 - k2);
+                double y = k1 * x + b1;
+            }
+        Console.WriteLine($"Red lines were crossed here: ({x}, {y})");
     }
-    else
-    {
-        double x = (b2 - b1) / (k1 - k2);
-        double y = k1 * x + b1;
-    }
-    Console.WriteLine($"Red lines were crossed here: ({x}, {y})");
-}
 
 Crossing();
-*/
+
